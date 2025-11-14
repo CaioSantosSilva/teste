ecde243661468abdc9f1cfa2d6c1e2c4a2a0f253
CaioSantosSilva <152020470+CaioSantosSilva@users.noreply.github.com>
2025-11-14 13:42:25 +0000

    Adicionar pagina Inicial

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function Home() {
+    return <h1>Ultimo Git commit com Push</h1>
+
+
+}
+
+export default Home;
+                                                                                                      
