9a69831fcb58b35a4802d01e9b97abeaccd4b702
CaioSantosSilva <152020470+CaioSantosSilva@users.noreply.github.com>
2025-11-18 11:33:15 +0000

    Adicionar  scipts `link:check`e `link:finx`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
 function Home() {
+  return <h1>Amor Te amoooooo quer café</h1>;
+}
 
+function teste2() {
+  console.log("Olá mundo novo");
+}
 
-  return <h1>Amor Te amoooooo quer café</h1>
+function teste3() {
+  console.log("Olá mundo novo");
+}
 
+function teste2() {
+  console.log("Olá mundo novo");
+}
 
+function teste2() {
+  console.log("Olá mundo novo");
 }
 
 export default Home;
-                                                                                                      
