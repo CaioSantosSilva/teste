77d02ce410908241c4a1272c5d25880049365ee1
CaioSantosSilva <152020470+CaioSantosSilva@users.noreply.github.com>
2025-11-18 12:01:31 +0000

    Adicionar o `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,4 @@
 function Home() {
   return <h1>Amor Te amoooooo quer café</h1>;
 }
-
-function teste2() {
-  console.log("Olá mundo novo");
-}
-
-function teste3() {
-  console.log("Olá mundo novo");
-}
-
-function teste2() {
-  console.log("Olá mundo novo");
-}
-
-function teste2() {
-  console.log("Olá mundo novo");
-}
-
 export default Home;
